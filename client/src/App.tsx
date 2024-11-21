e851c98c2906c33c2c81570f318bbc484f7215bf
david <kdavidmacharia8485@gmail.com>
2024-11-21 13:22:27 +0300

    updates on routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import Dashboard from "./dashboards/admin/Dashboard";
 import Patients from "./dashboards/admin/pages/patients";
 import Support from "./dashboards/admin/pages/support";
 import Doctors from "./dashboards/admin/pages/doctors";
-// import Activity from "./dashboards/admin/pages/activity";
 
 import AddDoctorForm from "./dashboards/admin/pages/addDoctorForm";
 import AddPatientForm from "./dashboards/admin/pages/addPatientForm";
@@ -26,12 +25,11 @@ const App: React.FC = () => {
           {/* Page Content */}
           <div className="flex-grow p-4">
             <Routes>
-              <Route path="/admin" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/admin/doctors" element={<Doctors />} />
               <Route path="/admin/patients" element={<Patients />} />
               <Route path="/admin/settings" element={<div>Settings Page</div>} />
-              {/* <Route path="/admin/activity" element={<Activity />} /> */}
-              <Route path="/support" element={<Support />} />
+              <Route path="/admin/support" element={<Support />} />
               <Route path="/admin/add-doctor" element={<AddDoctorForm />} />
               <Route path="/admin/add-patient" element={<AddPatientForm />} />
             </Routes>
--- client/src/dashboards/admin/Sidebar.tsx
+++ client/src/dashboards/admin/Sidebar.tsx
@@ -4,10 +4,10 @@ import { FaHome, FaUserMd, FaUsers, FaChartPie, FaCog, FaHeadset } from "react-i
 
 const Sidebar: React.FC = () => {
   const menuItems = [
-    { name: "Overview", icon: <FaHome />, path: "/admin" },
+    { name: "Overview", icon: <FaHome />, path: "/" },
     { name: "Doctors", icon: <FaUserMd />, path: "/admin/doctors" },
     { name: "Patients", icon: <FaUsers />, path: "/admin/patients" },
-    { name: "Activity", icon: <FaChartPie />, path: "/admin/activity" },
+    // { name: "Activity", icon: <FaChartPie />, path: "/admin/activity" },
     { name: "Settings", icon: <FaCog />, path: "/admin/settings" },
     { name: "Support", icon: <FaHeadset />, path: "/admin/support" },
   ];
