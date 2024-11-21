ee4d1f94949b578858732a1054121d7bc18347f5
david <kdavidmacharia8485@gmail.com>
2024-11-21 13:31:04 +0300

    removed unused code

--- client/src/dashboards/admin/Navbar.tsx
+++ client/src/dashboards/admin/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
+    <nav className="bg-white shadow-md px-0 py-0 flex justify-between items-center">
       <div className="flex items-center gap-4">
       </div>
     </nav>
