112eed5e47d899c4a9d20b477d2aaddb21d9aa6a
david <kdavidmacharia8485@gmail.com>
2024-11-30 15:52:32 +0300

    updates

--- client/src/dashboards/doctors/AppointmentsTable.tsx
+++ client/src/dashboards/doctors/AppointmentsTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getAppointments } from "../../api/appointmentApi";
 
 const AppointmentsTable = () => {
