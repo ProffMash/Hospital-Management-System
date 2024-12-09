7bf7f16a871d38ee4d3d646886256ec6fbdbe9f4
david <kdavidmacharia8485@gmail.com>
2024-12-09 07:57:44 +0300

    updates

--- client/src/Bot/Assistant.tsx
+++ client/src/Bot/Assistant.tsx
@@ -12,15 +12,15 @@ const BotpressChat: React.FC = () => {
       (window as any).botpressWebChat.init({
         composerPlaceholder: "Chat with Medinik Virtual Assistant",
         botConversationDescription: "Ask any Question",
-        botId: "d1a48b06-f29f-410f-940d-6d6e0939ab4c", 
+        botId: "d1a48b06-f29f-410f-940d-6d6e0939ab4c",
         hostUrl: "https://cdn.botpress.cloud/webchat/v1",
         messagingUrl: "https://messaging.botpress.cloud",
-        clientId: "f479a002-7a75-4ada-9a3d-1fecb53f4183", 
-        webhookId: "f479a002-7a75-4ada-9a3d-1fecb53f4183", 
+        clientId: "d1a48b06-f29f-410f-940d-6d6e0939ab4c",
+        webhookId: "f479a002-7a75-4ada-9a3d-1fecb53f4183",
         lazySocket: true,
         themeName: "prism",
-        botName: "Medinik Virtual Assistant",
-        avatarUrl: "https://i.pinimg.com/564x/3c/99/0c/3c990c67c30df3fdc95d1ed1371d21be.jpg", 
+        botName: "Vehicle Rental Assistant Bot",
+        avatarUrl: "https://i.pinimg.com/564x/3c/99/0c/3c990c67c30df3fdc95d1ed1371d21be.jpg",
         frontendVersion: "v1",
         useSessionStorage: true,
         enableConversationDeletion: true,
@@ -32,8 +32,6 @@ const BotpressChat: React.FC = () => {
 
     // Append the script element to the document body
     document.body.appendChild(script);
-    
-    // Clean up the script on component unmount
     return () => {
       document.body.removeChild(script);
     };
