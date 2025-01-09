63404bb9edc0e240affd748d7cf287471a72aca9
david <kdavidmacharia8485@gmail.com>
2025-01-09 23:49:37 +0300

    contact deployed api

--- client/src/api/contactApi.ts
+++ client/src/api/contactApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000/api/contacts/';
+const BASE_URL = 'https://hospital-m-s-backend.onrender.com/api/contacts/';
 
 export interface Contact {
   contact_id?: number; 
